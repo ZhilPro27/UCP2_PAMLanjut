605cea54de389fc730252a89fab757791e8b5a8b
ZhilPro27 <zhilalkrisna@gmail.com>
2026-05-12 12:59:46 +0700

    Menambahkan rute untuk registrasi pengguna pada kontroler otentikasi.

--- backend/src/routes/auth_route.js
+++ backend/src/routes/auth_route.js
@@ -4,5 +4,6 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/login', AuthController.login);
+router.post('/register', AuthController.register);
 
 export default router;
